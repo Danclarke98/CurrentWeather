c062774737025ac638bebf9fc902263caaea84d6
Daniel Clarke <danclarke98@gmail.com>
2019-08-01 17:21:03 +0100

    Celcius and Farenheit options

--- app.js
+++ app.js
@@ -1,52 +1,60 @@
-  window.addEventListener('load',()=>{
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description')
-    let temperatureDegree = document.querySelector('.temperature-degree')
-    let locationTZ = document.querySelector('.location-tz') 
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  const temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  const temperatureDegree = document.querySelector(".temperature-degree");
+  const locationTZ = document.querySelector(".location-tz");
+  const temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span  ");
 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                long = position.coords.longitude;
-                lat = position.coords.latitude;
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
+      const api = `${proxy}https://api.darksky.net/forecast/841c7ce9ad8fc6c0eac7b302c634d2c5/${lat},${long} `;
 
-                const proxy = `https://cors-anywhere.herokuapp.com/`;                 
-                const api = `${proxy}https://api.darksky.net/forecast/841c7ce9ad8fc6c0eac7b302c634d2c5/${lat},${long } `
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const { temperature, summary, icon } = data.currently;
+          //Set DOM elements from API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTZ.textContent = data.timezone;
 
-                fetch(api)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    const {temperature, summary, icon} = data.currently;
-                    //Set DOM elements from API
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = summary;
-                    locationTZ.textContent = data.timezone;
+          // Set Celcius
+          let celsius = temperature - 32 * (5 / 9);
 
-                    // Set Icon
-                    setIcons(icon, document.querySelector(".icon"));
+          // Set Icon
+          setIcons(icon, document.querySelector(".icon"));
 
-                });  
-    
+          // C/F Switcher
 
-            });
-            
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
+        });
+    });
+  } else {
+    alert("Please enable location services");
+  }
 
-    }else{
-        alert("Please enable location services");
-    }
-
-
-    function setIcons(icon, iconID){
-        const skycons = new Skycons({color: "white"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        skycons.play();
-        return skycons.set(iconID, Skycons[currentIcon]); 
-    }
-
-
-});  
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+});
