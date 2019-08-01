47aa411e140a826ff7010938be7ab37a0c453e4d
Daniel Clarke <danclarke98@gmail.com>
2019-08-01 16:15:05 +0100

    retrieve information using darksky API

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
- window.addEventListener('load',()=>{
+  window.addEventListener('load',()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description')
+    let temperatureDegree = document.querySelector('.temperature-degree')
+    let locationTZ = document.querySelector('.location-tz') 
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(
             position => {
                 long = position.coords.longitude;
                 lat = position.coords.latitude;
-            })
+
+                const proxy = `https://cors-anywhere.herokuapp.com/`;                 
+                const api = `${proxy}https://api.darksky.net/forecast/841c7ce9ad8fc6c0eac7b302c634d2c5/${lat},${long } `
+
+                fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const {temperature, summary} = data.currently;
+                    //Set DOM elements from API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTZ.textContent = data.timezone;
+
+                });  
+    
+
+            });
+            
+
     }else{
         alert("Please enable location services");
     }
-})
+
+});  
